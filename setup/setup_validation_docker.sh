879f10486e6cd0c3c4634f937d615725e16666a9
David Richer <david.richer@autodesk.com>
2017-06-21 21:10:07 -0400

    Fix indentation, replace tab by spaces

--- setup/setup_validation_docker.sh
+++ setup/setup_validation_docker.sh
@@ -33,21 +33,21 @@ function validate {
     MOSR=`rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release)`
 
     if (( $(echo "$MOSR > 6" |bc -l) )); then
-	export OVER="OK"
+        export OVER="OK"
     else
-	export OVER="BAD"
+        export OVER="BAD"
     fi
 
     # Os release checker
     if [ $(cat /etc/*release | grep -c CentOS) -ne 0 ]; then
-	export DISTRO='centos'
-	export OVER="OK"
+        export DISTRO='centos'
+        export OVER="OK"
     elif [ $(cat /etc/*release | grep -c 'Red Hat') -ne 0 ]; then
-	export DISTRO='redhat'
-	export OVER="OK"
+        export DISTRO='redhat'
+        export OVER="OK"
     else
-	export DISTRO="unknown"
-	export OVER="BAD"
+        export DISTRO="unknown"
+        export OVER="BAD"
     fi
     printf "RHEL 7/CentOS 7        : %-10s %-10s %-10d\n" $OVER $DISTRO $MOSR
 
@@ -56,9 +56,9 @@ function validate {
     CCV=`uname -r|awk -F. '{print $1"."$2}'`
 
     if (( $(echo "$CCV >= $DRV" |bc -l) )); then
-	export OVER="OK"
+        export OVER="OK"
     else
-	export OVER="BAD"
+        export OVER="BAD"
     fi
     printf "Kernel version         : %-10s %-10s\n" $OVER $CCV
 
@@ -69,9 +69,9 @@ function validate {
     # Number of CPU
     NCPU=`cat /proc/cpuinfo |grep "model name" |awk -F: '{print $2}'| wc -l`
     if (( $NCPU < 6)); then
-	export OVER="BAD"
+        export OVER="BAD"
     else
-	export OVER="OK"
+        export OVER="OK"
     fi
     printf "NB CPU                 : %-10s %-10d\n" $OVER $NCPU
 
@@ -80,7 +80,7 @@ function validate {
     TMEM=`echo "scale = 3; $MEM/1024/1024" |bc`
 
     if (( $MEM < 67108864 ));then
-	export OVER="BAD"
+        export OVER="BAD"
     else
         export OVER="OK"
     fi
@@ -89,7 +89,7 @@ function validate {
     # SELinux check
     export OVER="OK"
     if [ ! \( `getenforce` == "Permissive" -o `getenforce` == "Disabled" \) ]; then
-      export OVER="BAD"
+        export OVER="BAD"
     fi
     printf "SE Linux               : %-10s %-10s\n" $OVER `getenforce`
 
@@ -99,14 +99,14 @@ function validate {
     export OVER="BAD"
     DOV=`docker -v`
     if [ -n "$DOV" ];then
-	DOCK=`docker -v|awk '{print $3}'|awk -F. '{print $1}'`
-	if (( $DOCK < 17 ));then
-    	    export OVER="BAD"
-	else
-    	    export OVER="OK"
-	fi
+        DOCK=`docker -v|awk '{print $3}'|awk -F. '{print $1}'`
+        if (( $DOCK < 17 ));then
+            export OVER="BAD"
+        else
+            export OVER="OK"
+        fi
     else
-	echo "Docker is not installed. Please install." `basename "$0"` "--help"
+        echo "Docker is not installed. Please install." `basename "$0"` "--help"
     fi
     printf "Docker installed       : %-10s $DOV\n" $OVER
 
@@ -114,15 +114,15 @@ function validate {
     export OVER="BAD"
     DCOV=`docker-compose -v`
     if [ -n "$DCOV" ];then
-	DOCKC=`docker-compose -v|awk '{print $3}'|awk -F. '{print $1" "$2}'`
-	V=($DOCKC)
-	if (( ${V[0]} < 1 && ${V[1]} < 13 ));then
-	    export OVER="BAD"
-	else
-    	    export OVER="OK"
-	fi
+        DOCKC=`docker-compose -v|awk '{print $3}'|awk -F. '{print $1" "$2}'`
+        V=($DOCKC)
+        if (( ${V[0]} < 1 && ${V[1]} < 13 ));then
+            export OVER="BAD"
+        else
+            export OVER="OK"
+        fi
     else
-	echo "Docker-compose is not installed. Please install." `basename "$0"` "--help"
+        echo "Docker-compose is not installed. Please install." `basename "$0"` "--help"
     fi
     printf "Docker-compose         : %-10s $DCOV\n" $OVER
 
@@ -131,13 +131,13 @@ function validate {
     DRUN=`rpm -qa|grep docker`
     if [ -n "$DRUN" ]; then
     STATUS=`systemctl status docker.service|grep active|awk '{print $2}'`
-	if [ $STATUS = 'active' ];then
-	    export OVER="OK"
-	else
-	    export OVER="BAD"
-	fi
+        if [ $STATUS = 'active' ];then
+            export OVER="OK"
+        else
+            export OVER="BAD"
+        fi
     else
-	echo "Docker is not installed. Please install." `basename "$0"` "--help"
+        echo "Docker is not installed. Please install." `basename "$0"` "--help"
     fi
     printf "Docker-status          : %-10s %-10s\n" $OVER $STATUS
 
@@ -148,12 +148,12 @@ function validate {
     echo "==============="
     s3_hosts=(sg-media-usor-01 sg-media-tokyo sg-media-ireland)
     for i in ${s3_hosts[@]}; do
-	if ( curl -sS  https://${i}.s3.amazonaws.com|grep "AccessDenied">/dev/null );then
-	    export OVER="OK"
-	else
-	    export OVER="NO"
-	    break
-	fi
+        if ( curl -sS  https://${i}.s3.amazonaws.com|grep "AccessDenied">/dev/null );then
+            export OVER="OK"
+        else
+            export OVER="NO"
+            break
+        fi
     done
     printf "S3 available           : %-10s\n" $OVER
 
@@ -165,42 +165,42 @@ function validate {
     PGCHECK=`rpm -qa|grep postgres`
     if [ -n "$PGCHECK" ];then
     STATUS=`systemctl status postgresql.service|grep active|awk '{print $2}'`
-	if [ "$STATUS" = 'active' ];then
-	    export OVER="YES"
-	else
-	    export OVER="NO"
-	fi
+        if [ "$STATUS" = 'active' ];then
+            export OVER="YES"
+        else
+            export OVER="NO"
+        fi
     else
-	echo "Postgres is not installed."
+        echo "Postgres is not installed."
     fi
     printf "PostgreSQL-running     : %-10s %-10s\n" $OVER $STATUS
 
     # Postgres parameters
     if sudo test -f $PG_FILE; then 
-	printf "\nPostgres parameters:\n"
-	sudo cat $PG_FILE|grep -E 'shared_buffers|work_mem|maintenance_work_mem|vacuum_cost_delay|effective_cache_size|max_connections|statement_timeout'|uniq
+        printf "\nPostgres parameters:\n"
+        sudo cat $PG_FILE|grep -E 'shared_buffers|work_mem|maintenance_work_mem|vacuum_cost_delay|effective_cache_size|max_connections|statement_timeout'|uniq
     else
-	export OVER="NONE"
-	printf "Postgres parameters    : %-10s\n" $OVER
+        export OVER="NONE"
+        printf "Postgres parameters    : %-10s\n" $OVER
     fi
 }
 
 function install_all {
 
-sudo yum install -y yum-utils epel-release
+    sudo yum install -y yum-utils epel-release
 
-sudo yum-config-manager \
-    --add-repo \
-        https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum makecache fast
+    sudo yum-config-manager \
+        --add-repo \
+            https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum makecache fast
 
-# Install docker
-sudo yum -y install docker-ce python2-pip
-sudo systemctl start docker
-sudo docker run hello-world
+    # Install docker
+    sudo yum -y install docker-ce python2-pip
+    sudo systemctl start docker
+    sudo docker run hello-world
 
-# Install docker compose
-sudo pip install docker-compose
+    # Install docker compose
+    sudo pip install docker-compose
 }
 
 unset usage
