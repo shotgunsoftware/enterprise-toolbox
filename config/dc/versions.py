f14bd00cef67f80b1c770515ef1d3447b2142532
jing liu <jing.liu@autodesk.com>
2018-03-12 13:12:53 +0800

    add new version

--- config/dc/versions.py
+++ config/dc/versions.py
@@ -2,10 +2,10 @@
 app_supported=['7.7.1.1', '7.6.3.2', '7.6.3.1', '7.6.3.0', '7.5.2.0', '7.4.3.0', '7.3.3.0']
 
 '''transcoder server version list'''
-server_supported=['6.0.6','5.0.7']
+server_supported=['6.0.7', '6.0.6','5.0.7']
 
 '''transcoder workder version list'''
-worker_supported=['9.0.5','8.2.5']
+worker_supported=['10.1.1', '9.0.5','8.2.5']
 
 '''shotgun enterprise version list'''
 sec_supported=['1.2.3', '1.2.1']
