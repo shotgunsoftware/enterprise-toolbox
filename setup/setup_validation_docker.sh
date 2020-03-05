071c23090be03ac7641e3c66449ae5c84e0340d5
Sergei Kiktev <sergei.kiktev@autodesk.com>
2020-03-05 13:57:08 +1300

    A new way to install docker compose. Non pip installation.

--- setup/setup_validation_docker.sh
+++ setup/setup_validation_docker.sh
@@ -200,7 +200,10 @@ function install_all {
     sudo docker run hello-world
 
     # Install docker compose
-    sudo pip install docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    docker-compose --version
 }
 
 unset usage
